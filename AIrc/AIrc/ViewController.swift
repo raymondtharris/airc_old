6b0653e17ee5d4930903276eeacf0cba49afc190
Tim <raymondtimothyharris@gmail.com>
2015-07-04 10:07:11 -0700

    Initial commit
    Started iOS project this is the initial commit.

---
+++ AIrc/AIrc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AIrc
+//
+//  Created by Tim Harris on 7/4/15.
+//  Copyright © 2015 Tim Harris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ AIrc/AIrcUITests/AIrcUITests.swift
@@ -0,0 +1,35 @@
+//
+//  AIrcUITests.swift
+//  AIrcUITests
+//
+//  Created by Tim Harris on 7/4/15.
+//  Copyright © 2015 Tim Harris. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class AIrcUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
